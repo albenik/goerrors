5b084562881a9e28539df7fa714960d8a6770e5b
Veniamin Albaev <albenik@gmail.com>
2017-11-02 18:00:07 +0300

    Yet another interface change

--- errors.go
+++ errors.go
@@ -2,108 +2,84 @@ package errors
 
 import (
 	"bytes"
-	native "errors"
 	"fmt"
-	"io"
 	"runtime"
 )
 
-type Detailed interface {
+type DetailedError interface {
 	error
-	WithAnnotation(string) Detailed
-	CausedBy(error) Detailed
 	Origin() error
+	Inherit(error) DetailedError
+	CausedBy(error) DetailedError
 }
 
 type detailed struct {
-	origin     error
-	annotation string
-	file       string
-	line       int
-	fn         string
-	list       []error
+	text   string
+	file   string
+	line   int
+	origin error
+	cause  error
 }
 
-func (err *detailed) WithAnnotation(text string) Detailed {
-	err.annotation = text
-	return err
+func (e *detailed) Origin() error {
+	return e.origin
 }
 
-func (err *detailed) CausedBy(cause error) Detailed {
-	if err.list == nil {
-		err.list = make([]error, 0, 1)
+func (e *detailed) Inherit(other error) DetailedError {
+	switch err := other.(type) {
+	case DetailedError:
+		e.origin = err.Origin()
+		e.cause = other
+	default:
+		e.origin = err
 	}
-	err.list = append(err.list, cause)
-	return err
+	return e
 }
 
-func (err *detailed) Origin() error {
-	return err.origin
+func (e *detailed) CausedBy(other error) DetailedError {
+	e.cause = other
+	return e
 }
 
-func (err *detailed) Error() string {
-	buf := new(bytes.Buffer)
-	if len(err.annotation) > 0 {
-		buf.WriteString(err.annotation)
-		buf.WriteString(": ")
+func (e *detailed) Error() string {
+	buf := bytes.NewBufferString(e.text)
+	fmt.Fprintf(buf, " @ %s:%d", e.file, e.line)
+	if e.origin != nil && e.origin != Origin(e.cause) {
+		fmt.Fprint(buf, " inhertis ", e.origin.Error())
 	}
-	buf.WriteString(err.origin.Error())
-	for _, cause := range err.list {
-		buf.WriteString(" caused by ")
-		buf.WriteString(cause.Error())
+	if e.cause != nil {
+		fmt.Fprint(buf, " caused by ", e.cause.Error())
 	}
 	return buf.String()
 }
 
-func (err *detailed) Format(s fmt.State, verb rune) {
+func (e *detailed) Format(s fmt.State, verb rune) {
 	if verb == 'q' {
 		fmt.Fprint(s, "\"")
 	}
-	if len(err.annotation) > 0 {
-		io.WriteString(s, err.annotation)
-		io.WriteString(s, ": ")
-	}
-	io.WriteString(s, err.origin.Error())
-
 	switch verb {
 	case 'v', 's', 'q':
-		fmt.Fprintf(s, " @ %s:%d", err.file, err.line)
-		for _, cause := range err.list {
-			fmt.Fprintf(s, " caused by %v", cause)
-		}
+		fmt.Fprintf(s, e.Error())
 	default:
-		fmt.Fprintf(s, "%%%c(invalid for %T)", verb, err)
+		fmt.Fprint(s, "%", string(verb), "(invalid for error ", e.Error(), ")")
 	}
 	if verb == 'q' {
 		fmt.Fprint(s, "\"")
 	}
 }
 
-func withDetails(err error) *detailed {
-	pc, file, line, _ := runtime.Caller(2)
-	return &detailed{origin: err, file: trimGOPATH(file), line: line, fn: runtime.FuncForPC(pc).Name()}
+func New(text string) DetailedError {
+	_, file, line, _ := runtime.Caller(1)
+	return &detailed{text: text, file: trimGOPATH(file), line: line}
 }
 
-func Wrap(err error) Detailed {
-	switch e := err.(type) {
-	case Detailed:
-		return e
-	default:
-		return withDetails(err)
-	}
-}
-
-func New(text string) Detailed {
-	return withDetails(native.New(text))
-}
-
-func Newf(format string, a ...interface{}) Detailed {
-	return withDetails(fmt.Errorf(format, a...))
+func Newf(format string, a ...interface{}) DetailedError {
+	return New(fmt.Sprintf(format, a...))
 }
 
 func Origin(err error) error {
-	if det, ok := err.(Detailed); ok {
-		return Origin(det.Origin())
+	if e, ok := err.(DetailedError); ok {
+		return e.Origin()
 	} else {
 		return err
 	}
